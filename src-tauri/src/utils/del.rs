3657064b208c8d79030e51bc73c946ffddd4a1de
Eythrocyte3803 <2544390577@qq.com>
2026-05-25 19:00:56 +0900

    将删除逻辑改成 移动到回收站

--- src-tauri/src/utils/del.rs
+++ src-tauri/src/utils/del.rs
@@ -1,15 +1,11 @@
-use std::fs;
 use std::path::Path;
+use trash;
 
 #[tauri::command]
 pub async fn del_dir(path: String) -> Result<(), String> {
     let target_path = Path::new(&path);
     if target_path.exists() {
-        if target_path.is_dir() {
-            fs::remove_dir_all(target_path).map_err(|e| e.to_string())?;
-        } else {
-            fs::remove_file(target_path).map_err(|e| e.to_string())?;
-        }
+        trash::delete(target_path).map_err(|e| e.to_string())?;
     }
     Ok(())
 }
